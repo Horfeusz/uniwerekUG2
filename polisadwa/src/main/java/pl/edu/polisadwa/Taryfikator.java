bca0972c45c08e688318db17a9587f984dc23ab0
Horfeusz <horfeusz@gmail.com>
2017-05-14 12:34:50 +0200

    Test taryfikatora

--- polisadwa/src/main/java/pl/edu/polisadwa/Taryfikator.java
+++ polisadwa/src/main/java/pl/edu/polisadwa/Taryfikator.java
@@ -9,13 +9,34 @@ import pl.edu.polisadwa.ryzyko.RyzykoDTO;
 
 public class Taryfikator {
 
-	public void sumaSkladek(PolisaDTO polisa) {
+	private void checkPolisa(PolisaDTO polisa) {
 		Preconditions.checkArgument(polisa != null, "Nie podano danych polisy");
+		Preconditions.checkArgument(polisa.getRyzyka() != null, "Na polisie nie ma zainicjowanych ryzyk");
+	}
+
+	public void sumaSkladek(PolisaDTO polisa) {
+		checkPolisa(polisa);
 		BigDecimal skladka = BigDecimal.ZERO;
 		for (RyzykoDTO ryzyko : polisa.getRyzyka()) {
 			skladka = skladka.add(ryzyko.getSkladka());
 		}
 		polisa.setSkladka(skladka);
+	}
+
+	public void zwyzka(PolisaDTO polisa, int procentZwyzki) {
+		checkPolisa(polisa);
+		if (procentZwyzki == 0) {
+			return;
+		}
+		Preconditions.checkArgument(procentZwyzki > 0, "Wartość zwyżki musi być większa od zera");
+		double mnoznikTmp = procentZwyzki / 100d;
+		BigDecimal mnoznik = BigDecimal.valueOf(mnoznikTmp);
+		mnoznik = mnoznik.add(BigDecimal.ONE);
+		for (RyzykoDTO ryzyko : polisa.getRyzyka()) {
+			BigDecimal skladka = ryzyko.getSkladka().multiply(mnoznik);
+			ryzyko.setSkladka(skladka);
+		}
+		sumaSkladek(polisa);
 	}
 
 }
--- polisadwa/src/test/java/pl/edu/polisadwa/TaryfikatorTest.java
+++ polisadwa/src/test/java/pl/edu/polisadwa/TaryfikatorTest.java
@@ -16,6 +16,8 @@ import pl.edu.polisadwa.ryzyko.RyzykoDTO;
 public class TaryfikatorTest {
 
 	PolisaDTO polisa;
+	
+	List<RyzykoDTO> ryzyka = Lists.newArrayList();
 
 	static Taryfikator taryfikator;
 
@@ -27,26 +29,43 @@ public class TaryfikatorTest {
 	@Before
 	public void przed() {
 		polisa = new PolisaDTO();
-	}
-
-	@Test
-	public void sumaSkladekTest() {
-		List<RyzykoDTO> ryzyka = Lists.newArrayList();
-
+		
 		RyzykoDTO ryz1 = new RyzykoDTO();
 		ryz1.setSkladka(BigDecimal.TEN);
 		ryzyka.add(ryz1);
 
 		RyzykoDTO ryz2 = new RyzykoDTO();
-		ryz2.setSkladka(BigDecimal.ONE);
-		ryzyka.add(ryz2);
-
-		polisa.setRyzyka(ryzyka);
-
-		taryfikator.sumaSkladek(polisa);
-
-		Assert.assertEquals(polisa.getSkladka().doubleValue(), BigDecimal.valueOf(11d).doubleValue(), 0);
-
+		ryz2.setSkladka(BigDecimal.TEN);
+		ryzyka.add(ryz2);		
 	}
 
+	@Test
+	public void sumaSkladekTest() {
+		polisa.setRyzyka(ryzyka);
+		
+		taryfikator.sumaSkladek(polisa);
+
+		Assert.assertEquals(polisa.getSkladka().doubleValue(), BigDecimal.valueOf(20d).doubleValue(), 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void polisaNullTest() {
+		taryfikator.sumaSkladek(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void polisaBezRyzyk() {
+		taryfikator.sumaSkladek(polisa);
+	}
+	
+	@Test
+	public void zwyzkaTest() {
+		polisa.setRyzyka(ryzyka);
+		
+		taryfikator.zwyzka(polisa, 10);
+		
+		Assert.assertEquals(polisa.getSkladka().doubleValue(), BigDecimal.valueOf(22d).doubleValue(), 0);
+		
+	}
+	
 }
